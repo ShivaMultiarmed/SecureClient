81e2af50fd87c91b54010b662a18705a65c41ea9
Mikhail_Shell <theoutstandingme@gmail.com>
2025-02-12 19:52:03 +0700

    Скорректировал bat-файл

--- launch_two_clients.bat
+++ launch_two_clients.bat
@@ -1,2 +1,4 @@
+﻿@echo off
+chcp 65001 > nul
 start /b java -Dfile.encoding=UTF-8 -jar .\build\libs\SecureClient-1.0-SNAPSHOT.jar --name ALICE %1
 start /b java -Dfile.encoding=UTF-8 -jar .\build\libs\SecureClient-1.0-SNAPSHOT.jar --name BOB %1
