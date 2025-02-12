75ebf468e845ecdc8a9e852bfd4f9d2ed11a1a95
Mikhail_Shell <theoutstandingme@gmail.com>
2025-02-12 19:35:27 +0700

    added launching script

--- launch_two_clients.bat
+++ launch_two_clients.bat
@@ -1,2 +1,2 @@
-start /b java -jar .\build\libs\SecureClient-1.0-SNAPSHOT.jar --name ALICE --dfi
-start /b java -jar .\build\libs\SecureClient-1.0-SNAPSHOT.jar --name BOB --dfi
+start /b java -Dfile.encoding=UTF-8 -jar .\build\libs\SecureClient-1.0-SNAPSHOT.jar --name ALICE %1
+start /b java -Dfile.encoding=UTF-8 -jar .\build\libs\SecureClient-1.0-SNAPSHOT.jar --name BOB %1
