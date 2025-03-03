1ff8299d25c1d58fd0522cfbdecd6c0f9b9864c3
Mikhail_Shell <theoutstandingme@gmail.com>
2025-03-03 17:29:28 +0700

    commented bat launching file

--- launch_two_clients.bat
+++ launch_two_clients.bat
@@ -2,3 +2,7 @@
 chcp 65001 > nul
 start /b java -Dfile.encoding=UTF-8 -jar .\build\libs\SecureClient-1.0-SNAPSHOT.jar --name %2 %1 %4
 start /b java -Dfile.encoding=UTF-8 -jar .\build\libs\SecureClient-1.0-SNAPSHOT.jar --name %3 %1 %4
+
+:: параметр 1 - тип (--dhi, например)
+:: параметры 2, 3 - имена пользователей (CATHY, FRED)
+:: параметр 4 - подтип (--alg, --el)
--- src/main/kotlin/mikhail/shell/education/security/client/DiffieHellmanEllipticClient.kt
+++ src/main/kotlin/mikhail/shell/education/security/client/DiffieHellmanEllipticClient.kt
@@ -8,29 +8,29 @@ class DiffieHellmanEllipticClient(name: String): BaseEllipticClient(name) {
         client.webSocket("ws://127.0.0.1:9876/handshake") {
             send(Frame.Text(name))
             p = receiveNumber()
-            println("p = $p")
+            println("$name: p = $p")
             q = receiveNumber()
-            println("q = $q")
+            println("$name: q = $q")
             a = receiveNumber()
-            println("a = $a")
+            println("$name: a = $a")
             b = receiveNumber()
-            println("b = $b")
+            println("$name: b = $b")
             val xG = receiveNumber()
             val yG = receiveNumber()
             G = xG to yG
-            println("G = $G")
+            println("$name: G = $G")
             secretKey = generateSecretKey(q!!)
-            println("секретный ключ = $secretKey")
+            println("$name: секретный ключ = $secretKey")
             publicKey = G!!.evaluateComposition(secretKey!!, p!!, a!!)
-            println("свой открытый ключ = $publicKey")
+            println("$name: свой открытый ключ = $publicKey")
             sendNumber(publicKey!!.first)
             sendNumber(publicKey!!.second)
             val otherPublicKeyX = receiveNumber()
             val otherPublicKeyY = receiveNumber()
             val otherPublicKey = otherPublicKeyX to otherPublicKeyY
-            println("чужой открытый ключ = $otherPublicKey")
+            println("$name: чужой открытый ключ = $otherPublicKey")
             val sharedSecretKey = otherPublicKey.evaluateComposition(secretKey!!, p!!, a!!)
-            println("общий секретный ключ = $sharedSecretKey")
+            println("$name: общий секретный ключ = $sharedSecretKey")
         }
     }
 }
